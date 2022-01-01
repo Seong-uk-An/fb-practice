31a9d8d64b802ee1564c205905ceadc1449d6e64
Seong-uk-An <asu970602@naver.com>
2022-01-01 22:42:30 +0900

    update

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,66 @@
+import { authService, createUser, signIn } from "fb";
+import { useState } from "react";
+
 function Auth() {
-  return <span>Auth</span>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await createUser(authService, email, password);
+        //create a new account
+      } else {
+        data = await signIn(authService, email, password);
+        // log in
+      }
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={onChange}
+          required
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={onChange}
+          required
+        />
+        <input
+          type="submit"
+          value={newAccount ? "Create new account" : "Log in"}
+        />
+      </form>
+      <button>continue with google</button>
+      <button>continue with github</button>
+    </div>
+  );
 }
 
 export default Auth;
